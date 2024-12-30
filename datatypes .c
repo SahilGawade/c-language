eb9836a7f37483d2c506dfee57c1df17140684a7
SahilGawade <sahilgawade828@gmail.com>
2024-12-30 11:59:19 +0530

    Create datatypes .c

---
+++ datatypes .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    printf("a=%d\n",a);
+
+    float bal=25000;
+    printf("bal=%f\n",bal);
+
+    char  C='$';
+    printf("C=%C\n",C);
+    return 0;
+}
