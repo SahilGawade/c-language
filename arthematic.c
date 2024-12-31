502df80127126eb81de567d96241e39e9de35b9e
SahilGawade <sahilgawade828@gmail.com>
2024-12-31 11:03:58 +0530

    Update arthematic.c

--- arthematic.c
+++ arthematic.c
@@ -1,1 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    //add
+    int z=10;
+    int x=10;
+    int c=z+x;
+    printf("c=%d\n",c);
 
+    // sub
+  
+      int v=265;
+      int b=200;
+      int g=v-b;
+      printf("g=%d\n",g);
+
+      //mul
+
+      int k=5;
+      int j=5;
+      int m=k*j;
+      printf("m=%d\n",m);
+
+      //div
+      int t=10;
+      int  y=2;
+      int h=y/t;
+      printf("h=%d\n",h);
+
+      //incr
+
+      int n=19;
+      n++;
+
+      printf("n=%d\n",n);
+     
+
+      // dicrement
+
+      int d=40;
+      d--;
+      printf("d=%d\n",d);
+      
+
+    return 0;
+}
