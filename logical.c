82fe74353dfaa8a63f1a613b48fb2d5d565e6764
SahilGawade <sahilgawade828@gmail.com>
2024-12-31 11:44:48 +0530

    Update logical.c

--- logical.c
+++ logical.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{
 
+    int num1=10;
+    int num2=20;
+    
+
+    // and &&
+    printf("num1==num2 && num1<num2 %d\n",num1==num2 && num1<num2);
+//or
+    printf("num1==num2 || num1>num2%d\n",num1==num2 || num1>num2);
+    
+
+
+// not 
+printf("!(num1<num2)%d\n",num1<num2);
+return 0;
+}
