5bcadda741dc9c6b1ca537c757f0b96734896632
SahilGawade <sahilgawade828@gmail.com>
2024-12-31 11:14:37 +0530

    Update relational.c

--- relational.c
+++ relational.c
@@ -1,1 +1,27 @@
 
+#include<stdio.h>
+int main()
+{ 
+    int d=30;
+    int t=40;
+    int u=30;
+
+    // greater than
+    printf("d>t%d\n",d>t);
+    //------------------------------------------------------------------------------------
+
+    // less than
+      printf("d<t=%d\n",d<t); 
+
+      // equal to 
+       printf("d==u%d\n",d==t);
+
+       // not equal to
+
+    printf("d!=t%d\n",d!=t);
+       
+       
+
+    return 0;
+
+}
