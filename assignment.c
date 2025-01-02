2013efa6e7ad849c11dd42d8b7e1aea13a13d62e
SahilGawade <sahilgawade828@gmail.com>
2025-01-02 10:34:13 +0530

    Create assignment.c

---
+++ assignment.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    
+    //1] = equal to 
+    printf("a=%d\n",a);
+
+    //2] += add equal
+    a+=5; 
+    printf("a+=5 %d\n",a);
+
+    //3] -= sub equal
+    a-=5;
+    printf("a-=5 %d\n",a);
+
+    //4] *= mul equal
+    a*=5;
+    printf("a*=5 %d\n",a);
+
+    //5] /= div equal
+    a/=5;
+    printf("a/=5 %d\n",a);
+
+    //6] %=
+
+    
+   
+
+
+
+
+
+
+    return 0;
+
+}
