82b901517deed6db6b62b8d5cc28e9dc15bfde78
SahilGawade <sahilgawade828@gmail.com>
2025-01-03 10:55:01 +0530

    Create c1_if.c

---
+++ conditional/c1_if.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{  
+    int num;
+    printf("enter the val :");
+    scanf("%d",&num);
+    if(num>10){
+        printf("num=%d  is grater  than 10.",num);
+
+    }
+ printf("exiting..");
+
+
+
+
+    return 0;
+
+
+}
