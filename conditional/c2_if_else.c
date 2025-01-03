440dd41647d02731f61ca32b97204e86f3cd7c5a
SahilGawade <sahilgawade828@gmail.com>
2025-01-03 10:55:39 +0530

    Create c2_if_else.c

---
+++ conditional/c2_if_else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+
+int num;
+printf("enter the val to check num even or odd :");
+scanf("%d",&num);
+if(num%2==0){
+    printf("num=%d is even\n",num);
+    
+}else{
+    printf("num=%d is odd\n",num);
+}
+
+    return 0;
+}
