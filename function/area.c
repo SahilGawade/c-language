37c3a5fe4104cecc742cd927c454c6fb96ae0880
SahilGawade <sahilgawade828@gmail.com>
2025-01-20 11:53:40 +0530

    Create area.c

---
+++ function/area.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ 
+ int  areaofcircle(int b, int h);
+
+ int main(){
+
+int tringle;
+ int b=90;
+ int h=15;
+ int result=areaofcircle(b,h);
+printf("result=%d",result);
+
+    return 0;
+ }
+  int  areaofcircle(int b,int h){
+   
+
+
+    return (0.5)*b*h;
+  }
